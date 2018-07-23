4fcdae56b73ae366c2822afec213051701427787
Sethorax <info@sethorax.com>
2018-07-23 22:28:44 +0200

    Added TypeScript definition file.

---
+++ dist/preact-q-lightbox.d.ts
@@ -0,0 +1,20 @@
+export enum ItemType {
+    Image = 'image',
+}
+
+interface LightboxItem {
+    type: ItemType,
+    src: string,
+}
+
+interface LightboxItems {
+    [index: number]: LightboxItem
+}
+
+interface Props {
+    showArrows?: boolean,
+    transitionDuration?: number,
+}
+
+export function displayLightbox(items: LightboxItems, startIndex: number): void;
+export function configureLightbox(props: Props): void;
